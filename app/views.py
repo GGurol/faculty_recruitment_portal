0bf639b32ac3d69ee569e60a413388c57b7832ad
AAnimeshTripathy <aanimeshtripathy@gmail.com>
2024-04-09 04:11:43 +0530

    changes done

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , redirect
+from django.http import HttpResponse, HttpResponseNotFound
+from django.urls import reverse
 # Create your views here.
+
+def register(request):
+    context={}
+    return render(request,'app/register.html',context)
+
+def login(request):
+    pass
+def forgotPassword(request):
+    pass
