369034bb70dd4b92cc69e753b182ffd619a19ce1
AAnimeshTripathy <148396210+AAnimeshTripathy@users.noreply.github.com>
2024-05-17 01:22:24 +0530

    Update settings.py

--- Portal_MySQL/settings.py
+++ Portal_MySQL/settings.py
@@ -138,7 +138,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "aanimeshtripathy@gmail.com"
-EMAIL_HOST_PASSWORD = "utoe sunj qfvr nupg"
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
